f5a08659c49852cbba15625ab04a224ca6b84e4f
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-07-02 22:09:16 -0300

    alterada as queries para Criteria

--- src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -1,13 +1,20 @@
 package br.com.caelum.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.model.Loja;
 import br.com.caelum.model.Produto;
 
 @Repository
@@ -26,62 +33,64 @@ public class ProdutoDao {
 	}
 
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
+		// String jpql = "SELECT p FROM Produto p";
+		//
+		// if (categoriaId != null) {
+		// jpql += " JOIN FETCH p.categorias c WHERE c.id = :pCategoriaID AND";
+		// } else {
+		// jpql += " WHERE"; //senão ocorrerá erro de sintaxe SQL ao buscar por nome,
+		// por exemplo
+		// }
+		// if (lojaId != null) {
+		// jpql += " p.loja.id = :pLojaId AND";
+		// }
+		// if (!nome.isEmpty()) {
+		// jpql += " p.nome LIKE :pProdutoNome AND";
+		// }
+		// jpql += " 1 = 1";
+		//
+		// TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		//
+		// if (categoriaId != null)
+		// query.setParameter("pCategoriaID", categoriaId);
+		//
+		// if (lojaId != null)
+		// query.setParameter("pLojaId", lojaId);
+		//
+		// if (!nome.isEmpty())
+		// query.setParameter("pProdutoNome", nome);
+		//
+		// return query.getResultList();
+
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> root = query.from(Produto.class);
+
+		Path<String> nomePath = root.<String>get("nome");
+		Path<Integer> lojaPath = root.<Loja>get("loja").<Integer>get("id");
+		Path<Integer> categoriaPath = root.join("categorias").<Integer>get("id");
 		
-		String jpql = "SELECT p FROM Produto p";
-		
-		if (categoriaId != null) {
-			jpql += " JOIN FETCH p.categorias c WHERE c.id = :pCategoriaID AND";
-		} else {
-			jpql += " WHERE"; //senão ocorrerá erro de sintaxe SQL ao buscar por nome, por exemplo
+		List<Predicate> predicates = new ArrayList<>();
+
+		if (!nome.isEmpty()) {
+			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
+			predicates.add(nomeIgual);
 		}
 		if (lojaId != null) {
-			jpql += " p.loja.id = :pLojaId AND";
+			Predicate localIgual = criteriaBuilder.equal(lojaPath, lojaId);
+			predicates.add(localIgual);
 		}
-		if (!nome.isEmpty()) {
-			jpql += " p.nome LIKE :pProdutoNome AND";
+		if (categoriaId != null) {
+			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
+			predicates.add(categoriaIgual);
 		}
-		jpql += " 1 = 1";
+
+		//query.where(nomeIgual, localIgual, categoriaIgual); - Desta forma apresenta erro pois as variáveis são locais
+		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
 		
-		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
-		
-		if (categoriaId != null)
-		query.setParameter("pCategoriaID", categoriaId);
-		
-		if (lojaId != null)
-		query.setParameter("pLojaId", lojaId);
-		
-		if (!nome.isEmpty())
-		query.setParameter("pProdutoNome", nome);
-		
-		return query.getResultList();
-		
-//		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-//		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-//		Root<Produto> root = query.from(Produto.class);
-//
-//		Path<String> nomePath = root.<String> get("nome");
-//		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-//		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
-//
-//		List<Predicate> predicates = new ArrayList<>();
-//
-//		if (!nome.isEmpty()) {
-//			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-//			predicates.add(nomeIgual);
-//		}
-//		if (categoriaId != null) {
-//			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-//			predicates.add(categoriaIgual);
-//		}
-//		if (lojaId != null) {
-//			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-//			predicates.add(lojaIgual);
-//		}
-//
-//		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-//
-//		TypedQuery<Produto> typedQuery = em.createQuery(query);
-//		return typedQuery.getResultList();
+		TypedQuery<Produto> typedQuery = em.createQuery(query);
+
+		return typedQuery.getResultList();
 
 	}
 
