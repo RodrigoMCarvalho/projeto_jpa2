c80b31eebe0dd667a56d6d2ef0de0475d96e4010
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-07-05 19:46:59 -0300

    configurado o Hibernate Statistics

--- src/main/java/br/com/caelum/JpaConfigurator.java
+++ src/main/java/br/com/caelum/JpaConfigurator.java
@@ -6,9 +6,10 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -43,8 +44,14 @@ public class JpaConfigurator {
 		dataSource.setMaxPoolSize(10);
 		dataSource.setNumHelperThreads(15);
 		
-		return dataSource;
-				
+		return dataSource;		
+	}
+	
+	@Bean
+	public Statistics statistics(EntityManagerFactory emf ) {
+		SessionFactory factory = emf.unwrap(SessionFactory.class);
+		
+		return factory.getStatistics();
 	}
 
 	@Bean
@@ -64,6 +71,7 @@ public class JpaConfigurator {
 		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 		props.setProperty("hibernate.cache.use_second_level_cache", "true");
 		props.setProperty("hibernate.cache.use_query_cache", "true");
+		props.setProperty("hibernate.generate_statistics", "true");
 		props.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory");
 
 		entityManagerFactory.setJpaProperties(props);
